8edcd6f662777e8b1f6cd3438db00a6396477fad
Adam Ever-Hadani <adamhadani@gmail.com>
2012-08-28 15:55:13 -0700

    added the tastypie_ext.authentication module to source control

---
+++ tastypie_ext/authentication.py
@@ -0,0 +1,59 @@
+from django.contrib.auth import authenticate, login
+
+from tastypie.authentication import Authentication
+from tastypie.http import HttpUnauthorized
+
+class FacebookOAUTH2Authentication(Authentication):
+    """
+    Handles authentication delegating to Facebook OAuth 2.0,
+    using the Django-facebook package.
+    
+    See tastypie_ext.resources.GETAPIFacebookTokenAuthenticationResource
+    for more documentation on the typical flow using this.
+    
+    """
+
+    def is_authenticated(self, request, **kwargs):
+        """
+        Authenticate with facebook, and return
+        user upon success.
+        
+        """
+
+        # Make sure user supplied access token in request
+        try:
+            fb_access_token = request.GET['fb_access_token']
+        except KeyError:
+            return self._unauthorized()
+
+        # Authenticate with facebook
+        from open_facebook import OpenFacebook
+        from django_facebook.connect import connect_user
+
+        facebook = OpenFacebook(fb_access_token)
+
+        try:
+            if not facebook or \
+                not facebook.is_authenticated():
+                return self._unauthorized()
+        except:
+            return self._unauthorized()
+        
+        
+        # Facebook authenticated, now associate
+        # with own internal user, Creating a new user if 
+        # necessary.
+        action, user = connect_user(request, fb_access_token, facebook)
+
+        request.user = user
+        
+        print "action, user: ", action, user
+        
+        return True
+    
+
+    def _unauthorized(self):
+        response = HttpUnauthorized()
+        return response
+        
+    
